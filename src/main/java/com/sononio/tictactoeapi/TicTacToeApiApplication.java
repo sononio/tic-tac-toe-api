ca53ec1a05425a2315695e4ff68c48e157b84924
Night River <nightdevgame@gmail.com>
2023-12-07 02:27:10 +0300

    fix swagger-ui https behavior

--- src/main/java/com/sononio/tictactoeapi/TicTacToeApiApplication.java
+++ src/main/java/com/sononio/tictactoeapi/TicTacToeApiApplication.java
@@ -1,11 +1,14 @@
 package com.sononio.tictactoeapi;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 public class TicTacToeApiApplication {
 
     public static void main(String[] args) {
